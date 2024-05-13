ae76e9c65b953a039f68ebb0f9325e9d42eebaa9
sicajc <jackyyeh1999@gmail.com>
2024-05-13 22:12:47 +0800

    Testing router

--- HW3/hw3/router.h
+++ HW3/hw3/router.h
@@ -40,9 +40,10 @@ SC_MODULE(Router)
         for (;;)
         {
             // for every output ports
+            // search for every input port
             for (int output_port_dir = 0; output_port_dir < 5; output_port_dir++)
             {
-                // search for every input port
+                // if output port is busy, something to transfer
                 if (out_buf_busy[output_port_dir] == true)
                 {
                     out_req[output_port_dir].write(true);
@@ -55,14 +56,15 @@ SC_MODULE(Router)
                     {
                         // sends data out from the buffer
                         out_flit[output_port_dir].write(out_port_flit);
+                        // release the output buffer
                         out_buf_busy[output_port_dir] = false;
                         out_req[output_port_dir].write(false);
 
                         cout << "Router " << id << " sent flit " << out_port_flit << " to port " << output_port_dir << endl;
                     }
-                    else if (in_ack[output_port_dir] == true) // handshaked
+                    else if (in_ack[output_port_dir] == true && (out_ack[current_flit_source]==false)) // handshaked
                     {
-
+                        // send the flit to the output port, note must delay out_ack by 1 cycle
                         out_flit[output_port_dir].write(out_port_flit);
                         out_buf[output_port_dir] = in_buf[current_flit_source];
                         out_ack[current_flit_source].write(true);
@@ -75,7 +77,9 @@ SC_MODULE(Router)
                     }
                 }
                 else
-                { // output port avilable
+                {
+                    // output port avilable
+                    out_req[output_port_dir].write(false);
 
                     for (int input_buf_dir = 0; input_buf_dir < 5; input_buf_dir++)
                     {
@@ -95,16 +99,53 @@ SC_MODULE(Router)
                             int d_x = std::abs(cur_x - dst_x);
                             int d_y = std::abs(cur_y - dst_y);
 
+                            int cur_dir;
+
+                            // finding the direction
                             if (cur_x == dst_x && cur_y == dst_y)
                             {
                                 // destination reached
+                                cur_dir = CORE;
                             }
-                            else if (d_x != 0) // check if stop moving in x direction
+                            else if (d_x != 0) // check if reached
                             {
+                                // determine going east or west
+                                int cost_east = (4 + cur_x + 1) % 4 - dst_x;
+                                int cost_west = (4 + cur_x - 1) % 4 - dst_x;
+
+                                if (cost_east <= cost_west)
+                                {
+                                    cur_dir = EAST;
+                                }
+                                else
+                                {
+                                    cur_dir = WEST;
+                                }
                             }
                             else
                             {
+                                // destermine going north or south
+                                int cost_north = (4 + cur_y + 1) % 4 - dst_y;
+                                int cost_south = (4 + cur_y - 1) % 4 - dst_y;
 
+                                if (cost_north <= cost_south)
+                                {
+                                    cur_dir = NORTH;
+                                }
+                                else
+                                {
+                                    cur_dir = SOUTH;
+                                }
+                            }
+
+                            // see if the selected port header matches the port direction
+                            if (cur_dir == output_port_dir)
+                            {
+                                out_buf_busy[output_port_dir] = true;
+                                flit_sources[output_port_dir] = input_buf_dir;
+                                src_current_dir[input_buf_dir] = cur_dir;
+                                cout << "Router " << id << " sent flit " << out_buf[output_port_dir] << " to port " << output_port_dir << endl;
+                                break;
                             }
                         }
                     }
