e20979b12baa8108aa701f1ef7bada01d88a97bf
sicajc <jackyyeh1999@gmail.com>
2024-05-14 18:25:09 +0800

    4x4 NoC done

--- HW3/hw3/core.h
+++ HW3/hw3/core.h
@@ -3,221 +3,293 @@
 
 #include "systemc.h"
 #include "pe.h"
+#include <cstring>
+#include <helperfunction.h>
+#include <deque>
+using namespace std;
 
+// The core interfaces, build lookup table here!
 SC_MODULE(Core)
 {
-  sc_in<bool> rst;
-  sc_in<bool> clk;
-  // receive
-  sc_in<sc_lv<34>> flit_rx; // The input channel
-  sc_in<bool> req_rx;       // The request associated with the input channel
-  sc_out<bool> ack_rx;      // The outgoing ack signal associated with the input channel
-  // transmit
-  sc_out<sc_lv<34>> flit_tx; // The output channel
-  sc_out<bool> req_tx;       // The request associated with the output channel
-  sc_in<bool> ack_tx;        // The outgoing ack signal associated with the output channel
+    sc_in<bool> rst;
+    sc_in<bool> clk;
+    // receive
+    sc_in<sc_lv<34>> flit_rx; // The input channel
+    sc_in<bool> req_rx;       // The request associated with the input channel
+    sc_out<bool> ack_rx;      // The outgoing ack signal associated with the input channel
 
-  PE pe;
-  int id;
-  sc_trace_file *tf;
+    // transmit
+    sc_out<sc_lv<34>> flit_tx; // The output channel
+    sc_in<bool> ack_tx;        // The outgoing ack signal associated with the output channel
+    sc_out<bool> req_tx;       // The request associated with the output channel
 
-  Packet *p_tx, *p_rx;
-  sc_lv<34> data_tx, data_rx;
-  int data_tx_cnt;
+    PE pe;
 
-  // SC_CTOR( Core ) {
-  //     SC_METHOD( run );
-  //     pe.init(id);
-  //     sensitive << clk.pos();
-  //     std::cout << "Core" << id << std::endl;
-  // }
+    int id;
 
-  SC_CTOR(Core);
+    Packet *pkt_tx, *pkt_rx;
 
-  Core(sc_module_name name, int id, sc_trace_file *tf = nullptr) : sc_module(name), id(id)
-  {
-    SC_METHOD(run);
-    pe.init(id);
-    this->id = id;
-    this->tf = tf;
-    sensitive << clk.pos();
-    dont_initialize();
-    // std::cout << "Init: Core_" << id << std::endl;
+    // Trace file
+    sc_trace_file *tf;
 
-    if (tf != nullptr)
+    // global variable
+
+    void send_packet()
     {
-      sc_trace(tf, clk, "core_" + std::to_string(id) + ".clk");
-      sc_trace(tf, rst, "core_" + std::to_string(id) + ".rst");
-      sc_trace(tf, flit_rx, "core_" + std::to_string(id) + ".flit_rx");
-      sc_trace(tf, req_rx, "core_" + std::to_string(id) + ".req_rx");
-      sc_trace(tf, ack_rx, "core_" + std::to_string(id) + ".ack_rx");
-      sc_trace(tf, flit_tx, "core_" + std::to_string(id) + ".flit_tx");
-      sc_trace(tf, req_tx, "core_" + std::to_string(id) + ".req_tx");
-      sc_trace(tf, ack_tx, "core_" + std::to_string(id) + ".ack_tx");
-    }
-  }
+        int flit_counts = 0;
+        int num_of_flits;
+        
+        std::deque<float> datas_q;
 
-  sc_lv<32> float_2_sclv32(float number)
-  {
-    int *binary_representation = reinterpret_cast<int *>(&number);
-    sc_lv<32> result;
-    for (int i = 0; i < 32; ++i)
-    {
-      result[i] = (*binary_representation >> (31 - i)) & 0x1;
-    }
-    return result;
-  }
-  float sclv32_2_float(const sc_lv<32> &lv)
-  {
-    int int_representation = 0;
-    for (int i = 0; i < 32; ++i)
-    {
-      int_representation = (int_representation << 1) + lv[i].to_bool();
-    }
-    float result;
-    std::memcpy(&result, &int_representation, sizeof(float));
-    return result;
-  }
+        int src_id;
+        int dest_id;
 
-  int sclv4_2_int(const sc_lv<4> &lv)
-  {
-    int result = 0;
-    for (int i = 3; i >= 0; --i)
-    {
-      result = (result << 1) + lv[i].to_bool();
-    }
-    return result;
-  }
+        int done_sending_flag;
+        
 
-  void run()
-  {
-    if (rst.read() == 1)
-    {
-      ack_rx.write(0);
-      req_tx.write(0);
-      flit_tx.write(0);
-      // flit_tx.write(id); // for check connection
-      p_tx = nullptr;
-      p_rx = nullptr;
-      data_tx_cnt = 0;
-    }
-    else
-    {
-      // transmit
-      if (p_tx == nullptr)
-      {
-        flit_tx.write(0);
-        req_tx.write(0);
-        p_tx = pe.get_packet();
-        data_tx_cnt = 0;
-        if (p_tx != nullptr)
+        for (;;)
         {
-          std::cout << "Info: Core_" << id << " get packet (src:"
-                    << p_tx->source_id << " dst:" << p_tx->dest_id
-                    << " size:" << p_tx->datas.size() << ")" << std::endl;
-        }
-      }
-      else
-      { // p_tx is not nullptr
-        if (data_tx_cnt == 0)
-        {
-          data_tx[33] = 1; // BOP
-          data_tx[32] = 0; // EOP
-          data_tx.range(31, 28) = p_tx->source_id;
-          data_tx.range(27, 24) = p_tx->dest_id;
-          data_tx.range(23, 0) = 0;
-          flit_tx.write(data_tx);
-          req_tx.write(1);
-          data_tx_cnt = data_tx_cnt + 1;
-        }
-        else if (ack_tx.read() == 1)
-        {
-          if (data_tx_cnt > p_tx->datas.size())
-          {
-            delete p_tx;
-            p_tx = nullptr;
-            req_tx.write(0);
-            flit_tx.write(0);
-          }
-          // std::cout << "Core_" << id << " send data cnt:" << data_tx_cnt << std::endl;
-          else if (data_tx_cnt == p_tx->datas.size())
-          {
-            data_tx[33] = 0; // BOP
-            data_tx[32] = 1; // EOP
-            data_tx.range(31, 0) = float_2_sclv32(p_tx->datas[data_tx_cnt - 1]);
-            flit_tx.write(data_tx);
-          }
-          else
-          {
-            data_tx[33] = 0; // BOP
-            data_tx[32] = 0; // EOP
-            data_tx.range(31, 0) = float_2_sclv32(p_tx->datas[data_tx_cnt - 1]);
-            flit_tx.write(data_tx);
-          }
-          data_tx_cnt = data_tx_cnt + 1;
-        }
-      }
-      // receive
-      if (ack_rx == 1)
-      {
-        ack_rx.write(0);
-      }
-      else if (req_rx.read() == 1)
-      {
-        data_rx = flit_rx.read();
-        ack_rx.write(1);
-        if (data_rx[33] == 1)
-        { // BOP
-          if (p_rx != nullptr)
-            cout << "Core_" << id << " ERROR: BOP received while receiving packet" << endl;
-          else
-          {
-            p_rx = new Packet();
-            p_rx->source_id = sclv4_2_int(data_rx.range(31, 28));
-            p_rx->dest_id = sclv4_2_int(data_rx.range(27, 24));
-          }
-        }
-        else if (data_rx[32] == 1)
-        { // EOP
-          if (p_rx == nullptr)
-            cout << "Core_" << id << " ERROR: EOP received while not receiving packet" << endl;
-          else
-          {
-            p_rx->datas.push_back(sclv32_2_float(data_rx.range(31, 0)));
-            // std::cout << "Core_" << id << " receive " \
-            //           << sclv32_2_float(data_rx.range(31, 0)) << std::endl;
-
-            // print p_rx packet
-            std::cout << "Core_" << id << " check packet (src:"
-                      << p_rx->source_id << " dst:" << p_rx->dest_id
-                      << " size:" << p_rx->datas.size() << ")" << std::endl;
-            for (int i = 0; i < p_rx->datas.size(); i++)
+            if (rst.read() == true)
             {
-              std::cout << "Core_" << id << " data[" << i << "]:" << p_rx->datas[i] << std::endl;
+                // reset all output ports
+                req_tx.write(false);
+                flit_tx.write(0);
+                flit_counts = 0;
+                done_sending_flag = false;
+                pkt_tx == nullptr;
             }
-            // check packet
-            pe.check_packet(p_rx);
-            delete p_rx;
-            p_rx = nullptr;
-          }
+            else if(pkt_tx == nullptr)
+            {
+                
+                pkt_tx = pe.get_packet();
+                // need checking for null ptr to perform operation on pkt_tx since it is a pointer
+                if (pkt_tx != nullptr)
+                {
+                    
+                    vector<float> datas_f = pkt_tx->datas;
+                    // copy datas_f into data_q
+                    for (int i = 0; i < datas_f.size(); i++)
+                    {
+                        datas_q.push_back(datas_f[i]);
+                    }
+                    num_of_flits = datas_q.size() + 1; // 1 for additional header
+                    src_id = pkt_tx->source_id;
+                    dest_id = pkt_tx->dest_id;
+                    done_sending_flag = false;
+                    cout << "Core_" << id << " get packet" << "src_id:" << src_id << ", dest_id:" << dest_id << ", num_of_flits:" << num_of_flits << endl;
+                }
+                else
+                {
+                    vector<float> datas_f;
+                    num_of_flits = 0; // 1 for additional header
+                    src_id = 0;
+                    dest_id = 0;
+                    req_tx.write(false);
+                    flit_tx.write(0);
+                    flit_counts = 0;
+                    done_sending_flag = false;
+                    pkt_tx == nullptr;
+                }
+            } // pkt_tx is not nullptr
+            else if (flit_counts < num_of_flits)
+            {
+                // display
+                // cout << "Core_" << id << " send packet, num_of_flits:" << num_of_flits << endl;
+                // more data to send, send req to the router
+                req_tx.write(true);
+                if (flit_counts == 0) // header
+                {
+                    // cout << "Core_" << id << " send header" << endl;
+                    // send the header
+                    flit_size_t header = 0;
+                    header.range(33, 32) = 0b10;
+                    // src_id is 4 bits
+                    header.range(31, 28) = src_id;
+                    // dest_id is 4 bits
+                    header.range(27, 24) = dest_id;
+                    // rest 0
+                    header.range(23, 0) = 0;
+                    flit_tx.write(header);
+                    flit_counts++;
+                }
+                else if (ack_tx.read() == true && req_tx.read() == true)
+                {
+                    // cout << "Core_" << id << " send data" << endl;
+
+                    if (flit_counts == num_of_flits - 1)
+                    {
+                        // send the tail, dequeue the data
+                        float temp;
+                        temp = datas_q.front();
+                        datas_q.pop_front();
+                        // convert this temp data into sc_lv<32>
+                        flit_size_t tail = 0;
+                        tail.range(33, 32) = 0b01;
+                        tail.range(31, 0) = float_to_sc_lv(temp);
+
+                        // pop data out from the vector data
+                        flit_tx.write(tail);
+                    }
+                    else
+                    {
+                        // send the body
+                        float temp;
+                        temp = datas_q.front();
+                        datas_q.pop_front();
+                        // convert this temp data into sc_lv<32>
+                        flit_size_t body = 0;
+                        body.range(33, 32) = 0b00;
+                        body.range(31, 0) = float_to_sc_lv(temp);
+                        flit_tx.write(body);
+                    }
+
+                    flit_counts++;
+                    // cout << "Core_" << id << " send data cnt:" << flit_counts << endl;
+                }
+            }
+            else if (done_sending_flag == false && ack_tx.read() == true)
+            {
+                // no more data to send, stop sending request to router
+                req_tx.write(false);
+                flit_tx.write(0);
+                // display done sending signal
+                // cout << "Core_" << id << " done sending" << endl;
+                done_sending_flag = true;
+                pkt_tx = nullptr;
+                //reset
+                num_of_flits = 0; // 1 for additional header
+                src_id = 0;
+                dest_id = 0;
+                flit_counts = 0;
+                done_sending_flag = false;
+                pkt_tx == nullptr;
+            }
+
+            wait();
         }
-        else
-        {
-          p_rx->datas.push_back(sclv32_2_float(data_rx.range(31, 0)));
-          // std::cout << "Core_" << id << " receive " \
-          //           << sclv32_2_float(data_rx.range(31, 0)) << std::endl;
-        }
-      }
-      else
-      {
-        ack_rx.write(0);
-      }
     }
-    // Packet* p = pe.get_packet();
-    //  if (p != NULL) {
-    //      //pe.check_packet(p);
-    //  }
-  }
+
+    void receive_packet()
+    {
+        for (;;)
+        {
+            flit_size_t flit = flit_rx.read();
+            // ack receive
+            if (rst.read() == true)
+            {
+                ack_rx.write(false);
+            }
+            else if (ack_rx.read() == true)
+            {
+                ack_rx.write(false);
+            }
+            else if (req_rx.read() == true)
+            {
+                // send ack to the router, oscilates the ack_rx
+                if (flit.range(33, 32) == 0b10) // header
+                {
+                    ack_rx.write(true);
+                }
+                else if (flit.range(33, 32) == 0b01) // tail received
+                {
+                    ack_rx.write(true);
+                }
+                else
+                {
+                    ack_rx.write(true);
+                }
+            }
+            else
+            {
+                ack_rx.write(false);
+            }
+
+            if (req_rx.read() == true && ack_rx.read() == true)
+            {
+                // cout << "Core_" << id << " receive packet" << endl;
+                // receive the packet
+
+                // display flit read in
+                // cout << "Core_" << id << " receive flit:" << flit << endl;
+                // header
+
+                // send ack to the router, oscilates the ack_rx
+                if (flit.range(33, 32) == 0b10) // header
+                {
+                    pkt_rx = new Packet;
+
+                    if (pkt_rx == nullptr)
+                        cout << "Core_" << id << " ERROR: header received while receiving packet" << endl;
+                    else
+                    {
+                        pkt_rx->source_id = flit.range(31, 28).to_uint();
+                        pkt_rx->dest_id = flit.range(27, 24).to_uint();
+                        //cout << "Core_" << id << " receive header, src_id:" << pkt_rx->source_id << ", dest_id:" << pkt_rx->dest_id << endl;
+                    }
+                }
+                else if (flit.range(33, 32) == 0b01) // tail received
+                {
+                    // tail
+                    float temp = sc_lv_to_float(flit.range(31, 0));
+                    if (pkt_rx != nullptr)
+                        pkt_rx->datas.push_back(temp);
+                    cout << "Core_" << id << " receive tail, data:" << temp << endl;
+
+                    // display the whole pckt_rx->datas
+                    //cout << "pkt_rx->source_id:" << pkt_rx->source_id << ", pkt_rx->dest_id:" << pkt_rx->dest_id << endl;
+
+                    // for (int i = 0; i < pkt_rx->datas.size(); i++)
+                    // {
+                    //     cout << "Core_" << id << " receive data:" << pkt_rx->datas[i] << endl;
+                    // }
+
+                    // check the packet
+                    pe.check_packet(pkt_rx);
+                }
+                else
+                {
+                    // body
+                    float temp = sc_lv_to_float(flit.range(31, 0));
+                    if (pkt_rx != nullptr)
+                        pkt_rx->datas.push_back(temp);
+                    // cout << "Core_" << id << " receive body, data:" << temp << endl;
+                }
+            }
+
+            wait();
+        }
+    }
+
+    //=============================================================================
+    // For File dumping & Constructor
+    //=============================================================================
+    SC_CTOR(Core);
+
+    Core(sc_module_name name, int id, sc_trace_file *tf = nullptr) : sc_module(name), id(id)
+    {
+        // constructor
+        pe.init(id);
+        this->id = id;
+        this->tf = tf;
+
+        SC_THREAD(send_packet);
+        dont_initialize();
+        sensitive << clk.pos();
+
+        SC_THREAD(receive_packet);
+        dont_initialize();
+        sensitive << clk.pos();
+
+        // trace and dump the signals in a hierarchical way
+        sc_trace(tf, clk, "core_" + to_string(id) + ".clk"); // the dump signal would be named as core_0.clk
+        sc_trace(tf, rst, "core_" + to_string(id) + ".rst");
+        sc_trace(tf, flit_rx, "core_" + to_string(id) + ".flit_rx");
+        sc_trace(tf, req_rx, "core_" + to_string(id) + ".req_rx");
+        sc_trace(tf, ack_rx, "core_" + to_string(id) + ".ack_rx");
+        sc_trace(tf, flit_tx, "core_" + to_string(id) + ".flit_tx");
+        sc_trace(tf, req_tx, "core_" + to_string(id) + ".req_tx");
+        sc_trace(tf, ack_tx, "core_" + to_string(id) + ".ack_tx");
+    }
 };
 
 #endif
--- HW3/hw3/main.cpp
+++ HW3/hw3/main.cpp
@@ -1,161 +1,131 @@
 #include "systemc.h"
 #include "clockreset.h"
+#include "USER_DEFINED_PARAM.h"
 #include "core.h"
+#include "pe.h"
 #include "router.h"
+#define SIM_TIME 250000
 
-using namespace sc_core;
-
-enum Direction {DIR_C = 0, DIR_E = 1, DIR_W = 2, DIR_S = 3, DIR_N = 4};
-int sc_main(int argc, char* argv[])
+int sc_main(int argc, char *argv[])
 {
-  // =======================
-  //   signals declaration
-  // =======================
-  sc_signal < bool > clk;
-  sc_signal < bool > rst;
+    // =======================
+    //   signals declaration
+    // =======================
+    sc_signal<bool> clk;
+    sc_signal<bool> rst;
 
-  sc_signal < sc_lv<34> > flit_rx;
-  sc_signal < bool > req_rx;
-  sc_signal < bool > ack_rx;
+    sc_signal<sc_lv<34>> c_r_flit[4][4][2];
+    sc_signal<bool> c_r_req[4][4][2];
+    sc_signal<bool> c_r_ack[4][4][2];
 
-  sc_signal < sc_lv<34> > flit_tx;
-  sc_signal < bool > req_tx;
-  sc_signal < bool > ack_tx;
+    sc_signal<sc_lv<34>> r_r_flit_h[4][4][2];
+    sc_signal<bool> r_r_req_h[4][4][2];
+    sc_signal<bool> r_r_ack_h[4][4][2];
 
-  sc_signal < sc_lv<34> > wire_v_flit[4][4][2];
-  sc_signal < bool > wire_v_req[4][4][2];
-  sc_signal < bool > wire_v_ack[4][4][2];
+    sc_signal<sc_lv<34>> r_r_flit_v[4][4][2];
+    sc_signal<bool> r_r_req_v[4][4][2];
+    sc_signal<bool> r_r_ack_v[4][4][2];
 
-  sc_signal < sc_lv<34> > wire_h_flit[4][4][2];
-  sc_signal < bool > wire_h_req[4][4][2];
-  sc_signal < bool > wire_h_ack[4][4][2];
+    // Trace file declration for waveform dump
+    sc_trace_file *tf = sc_create_vcd_trace_file("wave");
+    sc_trace(tf, clk, "clk");
+    sc_trace(tf, rst, "rst");
 
-  sc_signal < sc_lv<34> > wire_c_flit[4][4][2];
-  sc_signal < bool > wire_c_req[4][4][2];
-  sc_signal < bool > wire_c_ack[4][4][2];
+    // =======================
+    //   modules declaration
+    // =======================
+    Clock m_clock("m_clock", 10);
+    Reset m_reset("m_reset", 15);
 
-  // =======================
-  //   waveform trace file
-  // =======================
-  sc_trace_file *tf = sc_create_vcd_trace_file("wave");
-  sc_trace(tf, clk, "clk");
-  sc_trace(tf, rst, "rst");
+    // instantiate 16 cores and 16 routers as 4x4 array
+    Core *m_core[4][4];
+    Router *m_router[4][4];
 
-
-  // =======================
-  //   modules declaration
-  // =======================
-  Clock m_clock("m_clock", 10);
-  Reset m_reset("m_reset", 15);
-
-  Core ***m_core;
-  Router ***m_router;
-
-  m_core = (Core ***) malloc(4 * sizeof(Core **));
-  m_router = (Router ***) malloc(4 * sizeof(Router **));
-
-  for (int i = 0; i < 4; i++) {
-    m_core[i] = (Core **) malloc(4 * sizeof(Core *));
-    m_router[i] = (Router **) malloc(4 * sizeof(Router *));
-  }
-
-  for (int i = 0; i < 4; i++) {
-    for (int j = 0; j < 4; j++) {
-      int id = i * 4 + j;
-      m_core[i][j] = new Core(("m_core_" + std::to_string(id)).c_str(), id, tf);
-      m_router[i][j] =  new Router(("m_router_" + std::to_string(id)).c_str(), id, tf);
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            int id = i * 4 + j;
+            // Router(sc_module_name name, int id, sc_trace_file *tf = nullptr) : sc_module(name), id(id)
+            m_core[i][j] = new Core(("m_core_" + to_string(id)).c_str(), id, tf);
+            m_router[i][j] = new Router(("m_router_" + to_string(id)).c_str(), id, tf);
+        }
     }
-  }
 
+    // =======================
+    //   modules connection
+    // =======================
+    m_clock(clk);
+    m_reset(rst);
 
-  // =======================
-  //   modules connection
-  // =======================
-  // Clock and Reset
-  m_clock( clk );
-  m_reset( rst );
+    // connect all routers with cores
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            // Connect rst and clk
+            m_core[i][j]->clk(clk);
+            m_core[i][j]->rst(rst);
+            m_router[i][j]->clk(clk);
+            m_router[i][j]->rst(rst);
 
-  // Connecting m_core since it is an array, use in direct to connect ports
-  for (int i = 0; i < 4; i++) {
-    for (int j = 0; j < 4; j++) {
-      m_core[i][j]->clk( clk );
-      m_core[i][j]->rst( rst );
-      m_router[i][j]->clk( clk );
-      m_router[i][j]->rst( rst );
+            // Connect Core & routers
+            m_core[i][j]->flit_rx(c_r_flit[i][j][0]);
+            m_core[i][j]->req_rx(c_r_req[i][j][0]);
+            m_core[i][j]->ack_rx(c_r_ack[i][j][0]);
+
+            m_core[i][j]->flit_tx(c_r_flit[i][j][1]);
+            m_core[i][j]->req_tx(c_r_req[i][j][1]);
+            m_core[i][j]->ack_tx(c_r_ack[i][j][1]);
+
+            m_router[i][j]->out_flit[CORE](c_r_flit[i][j][0]);
+            m_router[i][j]->out_req[CORE](c_r_req[i][j][0]);
+            m_router[i][j]->in_ack[CORE](c_r_ack[i][j][0]);
+
+            m_router[i][j]->in_flit[CORE](c_r_flit[i][j][1]);
+            m_router[i][j]->in_req[CORE](c_r_req[i][j][1]);
+            m_router[i][j]->out_ack[CORE](c_r_ack[i][j][1]);
+
+            // Connect Routers Vertically
+            m_router[i][j]->out_flit[EAST](r_r_flit_h[i][j][0]);
+            m_router[i][j]->out_req[EAST](r_r_req_h[i][j][0]);
+            m_router[i][j]->in_ack[EAST](r_r_ack_h[i][j][0]);
+
+            m_router[i][j]->out_flit[WEST](r_r_flit_h[i][j][1]);
+            m_router[i][j]->out_req[WEST](r_r_req_h[i][j][1]);
+            m_router[i][j]->in_ack[WEST](r_r_ack_h[i][j][1]);
+
+            // Connect Routers Horizontally
+            m_router[i][j]->in_flit[EAST](r_r_flit_h[i][(j + 1) % 4][1]);
+            m_router[i][j]->in_req[EAST](r_r_req_h[i][(j + 1) % 4][1]);
+            m_router[i][j]->out_ack[EAST](r_r_ack_h[i][(j + 1) % 4][1]);
+
+            m_router[i][j]->in_flit[WEST](r_r_flit_h[i][(j + 3) % 4][0]);
+            m_router[i][j]->in_req[WEST](r_r_req_h[i][(j + 3) % 4][0]);
+            m_router[i][j]->out_ack[WEST](r_r_ack_h[i][(j + 3) % 4][0]);
+
+            // Connect Routers Horizontally
+            m_router[i][j]->out_flit[SOUTH](r_r_flit_v[i][j][0]);
+            m_router[i][j]->out_req[SOUTH](r_r_req_v[i][j][0]);
+            m_router[i][j]->in_ack[SOUTH](r_r_ack_v[i][j][0]);
+
+            m_router[i][j]->out_flit[NORTH](r_r_flit_v[i][j][1]);
+            m_router[i][j]->out_req[NORTH](r_r_req_v[i][j][1]);
+            m_router[i][j]->in_ack[NORTH](r_r_ack_v[i][j][1]);
+
+            m_router[i][j]->in_flit[SOUTH](r_r_flit_v[(i + 1) % 4][j][1]);
+            m_router[i][j]->in_req[SOUTH](r_r_req_v[(i + 1) % 4][j][1]);
+            m_router[i][j]->out_ack[SOUTH](r_r_ack_v[(i + 1) % 4][j][1]);
+
+            m_router[i][j]->in_flit[NORTH](r_r_flit_v[(i + 3) % 4][j][0]);
+            m_router[i][j]->in_req[NORTH](r_r_req_v[(i + 3) % 4][j][0]);
+            m_router[i][j]->out_ack[NORTH](r_r_ack_v[(i + 3) % 4][j][0]);
+        }
     }
-  }
 
-  // Router <-> Core
-  for (int i = 0; i < 4; i++) {
-    for (int j = 0; j < 4; j++) {
-      m_core[i][j]->flit_rx( wire_c_flit[i][j][0] );
-      m_core[i][j]->req_rx( wire_c_req[i][j][0] );
-      m_core[i][j]->ack_rx( wire_c_ack[i][j][0] );
+    // set simulation end time
+    sc_start(SIM_TIME, SC_NS);
 
-      m_core[i][j]->flit_tx( wire_c_flit[i][j][1] );
-      m_core[i][j]->req_tx( wire_c_req[i][j][1] );
-      m_core[i][j]->ack_tx( wire_c_ack[i][j][1] );
-
-      m_router[i][j]->out_flit[DIR_C]( wire_c_flit[i][j][0] );
-      m_router[i][j]->out_req[DIR_C]( wire_c_req[i][j][0] );
-      m_router[i][j]->in_ack[DIR_C]( wire_c_ack[i][j][0] );
-
-      m_router[i][j]->in_flit[DIR_C]( wire_c_flit[i][j][1] );
-      m_router[i][j]->in_req[DIR_C]( wire_c_req[i][j][1] );
-      m_router[i][j]->out_ack[DIR_C]( wire_c_ack[i][j][1] );
-    }
-  }
-
-  // Router <-> Router vertical connections
-  for (int i = 0; i < 4; i++) {
-    for (int j = 0; j < 4; j++) {
-      m_router[i][j]->out_flit[DIR_S]( wire_v_flit[i][j][0] );
-      m_router[i][j]->out_req[DIR_S]( wire_v_req[i][j][0] );
-      m_router[i][j]->in_ack[DIR_S]( wire_v_ack[i][j][0] );
-
-      m_router[i][j]->out_flit[DIR_N]( wire_v_flit[i][j][1] );
-      m_router[i][j]->out_req[DIR_N]( wire_v_req[i][j][1] );
-      m_router[i][j]->in_ack[DIR_N]( wire_v_ack[i][j][1] );
-
-      m_router[i][j]->in_flit[DIR_S]( wire_v_flit[(i + 1) % 4][j][1] );
-      m_router[i][j]->in_req[DIR_S]( wire_v_req[(i + 1) % 4][j][1] );
-      m_router[i][j]->out_ack[DIR_S]( wire_v_ack[(i + 1) % 4][j][1] );
-
-      m_router[i][j]->in_flit[DIR_N]( wire_v_flit[(i + 3) % 4][j][0] );
-      m_router[i][j]->in_req[DIR_N]( wire_v_req[(i + 3) % 4][j][0] );
-      m_router[i][j]->out_ack[DIR_N]( wire_v_ack[(i + 3) % 4][j][0] );
-    }
-  }
-
-  // Router <-> Router (horizontal)
-  for (int i = 0; i < 4; i++) {
-    for (int j = 0; j < 4; j++) {
-      m_router[i][j]->out_flit[DIR_E]( wire_h_flit[i][j][0] );
-      m_router[i][j]->out_req[DIR_E]( wire_h_req[i][j][0] );
-      m_router[i][j]->in_ack[DIR_E]( wire_h_ack[i][j][0] );
-
-      m_router[i][j]->out_flit[DIR_W]( wire_h_flit[i][j][1] );
-      m_router[i][j]->out_req[DIR_W]( wire_h_req[i][j][1] );
-      m_router[i][j]->in_ack[DIR_W]( wire_h_ack[i][j][1] );
-
-      m_router[i][j]->in_flit[DIR_E]( wire_h_flit[i][(j + 1) % 4][1] );
-      m_router[i][j]->in_req[DIR_E]( wire_h_req[i][(j + 1) % 4][1] );
-      m_router[i][j]->out_ack[DIR_E]( wire_h_ack[i][(j + 1) % 4][1] );
-
-      m_router[i][j]->in_flit[DIR_W]( wire_h_flit[i][(j + 3) % 4][0] );
-      m_router[i][j]->in_req[DIR_W]( wire_h_req[i][(j + 3) % 4][0] );
-      m_router[i][j]->out_ack[DIR_W]( wire_h_ack[i][(j + 3) % 4][0] );
-    }
-  }
-  // =======================
-  //   start simulation
-  // =======================
-  sc_start(1, SC_MS);
-
-
-  // =======================
-  //   end simulation
-  // =======================
-  sc_close_vcd_trace_file(tf);
-  return 0;
+    sc_close_vcd_trace_file(tf);
+    return 0;
 }
