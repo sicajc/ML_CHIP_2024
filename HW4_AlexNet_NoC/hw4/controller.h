dbba5bc8f391c39b96c1440b1796f624eb57a4c3
Jacky Yeh <jackyyeh1999@gmail.com>
2024-06-02 21:53:28 +0800

    Port connected, start coding Controller

--- HW4_AlexNet_NoC/hw4/controller.h
+++ HW4_AlexNet_NoC/hw4/controller.h
@@ -2,31 +2,72 @@
 #define CONTROLLER_H
 
 #include "systemc.h"
+using namespace std;
 
-SC_MODULE( Controller ) {
-    sc_in  < bool >  rst;
-    sc_in  < bool >  clk;
-    
+SC_MODULE(Controller)
+{
+    sc_in<bool> rst;
+    sc_in<bool> clk;
+
     // to ROM
-    sc_out < int >   layer_id;       // '0' means input data
-    sc_out < bool >  layer_id_type;  // '0' means weight, '1' means bias (for layer_id == 0, we don't care this signal)
-    sc_out < bool >  layer_id_valid;
+    sc_out<int> layer_id;       // '0' means input data
+    sc_out<bool> layer_id_type; // '0' means weight, '1' means bias (for layer_id == 0, we don't care this signal)
+    sc_out<bool> layer_id_valid;
 
     // from ROM
-    sc_in  < float > data;
-    sc_in  < bool >  data_valid;
-    
+    sc_in<float> data;
+    sc_in<bool> data_valid;
+
     // to router0
-    sc_out < sc_lv<34> > flit_tx;
-    sc_out < bool > req_tx;
-    sc_in  < bool > ack_tx;
+    sc_out<sc_lv<34>> flit_tx;
+    sc_out<bool> req_tx;
+    sc_in<bool> ack_tx;
 
     // from router0
-    sc_in  < sc_lv<34> > flit_rx;
-    sc_in  < bool > req_rx;
-    sc_out < bool > ack_rx;
+    sc_in<sc_lv<34>> flit_rx;
+    sc_in<bool> req_rx;
+    sc_out<bool> ack_rx;
+
+    // Trace file
+    sc_trace_file *tf;
 
 
+    void run()
+    {
+        for (;;)
+        {
+
+            wait();
+        }
+    }
+
+    //=============================================================================
+    // For File dumping & Constructor
+    //=============================================================================
+    SC_CTOR(Controller);
+
+    Controller(sc_module_name name, sc_trace_file *tf = nullptr) : sc_module(name)
+    {
+        // Constructor
+
+        // trace signals
+        sc_trace(tf, rst,   "m_controller.rst");
+        sc_trace(tf, clk,   "m_controller.clk");
+
+        sc_trace(tf, layer_id,   "m_controller.layer_id");
+        sc_trace(tf, layer_id_type,   "m_controller.layer_id_type");
+        sc_trace(tf, layer_id_valid,   "m_controller.layer_id_valid");
+
+        sc_trace(tf, data,   "m_controller.data");
+        sc_trace(tf, data_valid,   "m_controller.data_valid");
+
+        sc_trace(tf, flit_tx,   "m_controller.flit_tx");
+        sc_trace(tf, req_tx,   "m_controller.req_tx");
+        sc_trace(tf, ack_tx,   "m_controller.ack_tx");
+
+        sc_trace(tf, flit_rx,   "m_controller.flit_rx");
+        sc_trace(tf, req_rx,   "m_controller.req_rx");
+        sc_trace(tf, ack_rx,   "m_controller.ack_rx");
+    }
 };
-
 #endif
--- HW4_AlexNet_NoC/hw4/main.cpp
+++ HW4_AlexNet_NoC/hw4/main.cpp
@@ -2,10 +2,10 @@
 #include "clockreset.h"
 #include "USER_DEFINED_PARAM.h"
 #include "core.h"
-#include "pe.h"
 #include "router.h"
 #include "controller.h"
 #include "ROM.h"
+using namespace std;
 #define SIM_TIME 250000
 
 int sc_main(int argc, char *argv[])
@@ -16,12 +16,12 @@ int sc_main(int argc, char *argv[])
     sc_signal<bool> clk;
     sc_signal<bool> rst;
 
-    sc_signal <int> layer_id;
-    sc_signal <bool> layer_id_type;
-    sc_signal <bool> layer_id_valid;
+    sc_signal<int> layer_id;
+    sc_signal<bool> layer_id_type;
+    sc_signal<bool> layer_id_valid;
 
-    sc_signal <float> data;
-    sc_signal <bool> data_valid;
+    sc_signal<float> data;
+    sc_signal<bool> data_valid;
 
     sc_signal<sc_lv<34>> c_r_flit[4][4][2];
     sc_signal<bool> c_r_req[4][4][2];
@@ -34,6 +34,10 @@ int sc_main(int argc, char *argv[])
     sc_signal<sc_lv<34>> r_r_flit_v[4][4][2];
     sc_signal<bool> r_r_req_v[4][4][2];
     sc_signal<bool> r_r_ack_v[4][4][2];
+
+    sc_signal<sc_lv<34>> dummy_flit[2];
+    sc_signal<bool> dummy_req[2];
+    sc_signal<bool> dummy_ack[2];
 
     // Trace file declration for waveform dump
     sc_trace_file *tf = sc_create_vcd_trace_file("wave");
@@ -50,7 +54,7 @@ int sc_main(int argc, char *argv[])
     Core *m_core[4][4];
     Router *m_router[4][4];
     Controller *m_controller;
-
+    ROM *m_ROM;
 
     for (int i = 0; i < 4; i++)
     {
@@ -63,8 +67,8 @@ int sc_main(int argc, char *argv[])
         }
     }
 
-    m_controller = new Controller(("m_controller").c_str(), tf);
-    m_ROM        = new ROM(("m_ROM").c_str(), tf);
+    m_controller = new Controller(("m_controller"), tf);
+    m_ROM = new ROM(("m_ROM"), tf);
 
     // =======================
     //   modules connection
@@ -88,6 +92,12 @@ int sc_main(int argc, char *argv[])
     {
         for (int j = 0; j < 4; j++)
         {
+            // Connect rst and clk
+            m_core[i][j]->clk(clk);
+            m_core[i][j]->rst(rst);
+            m_router[i][j]->clk(clk);
+            m_router[i][j]->rst(rst);
+
             if (i == 0 && j == 0)
             {
                 // Core0 is reserved for Controller
@@ -104,15 +114,15 @@ int sc_main(int argc, char *argv[])
                 m_controller->data(data);
                 m_controller->data_valid(data_valid);
 
-                // To router0
-                m_controller->flit_tx(c_r_flit[i][j][0]);
-                m_controller->req_tx(c_r_req[i][j][0]);
-                m_controller->ack_tx(c_r_ack[i][j][0]);
-
                 // From router0
-                m_controller->flit_rx(c_r_flit[i][j][1]);
-                m_controller->req_rx(c_r_req[i][j][1]);
-                m_controller->ack_rx(c_r_ack[i][j][1]);
+                m_controller->flit_rx(c_r_flit[0][0][0]);
+                m_controller->req_rx(c_r_req[0][0][0]);
+                m_controller->ack_rx(c_r_ack[0][0][0]);
+
+                // To router0
+                m_controller->flit_tx(c_r_flit[0][0][1]);
+                m_controller->req_tx(c_r_req[0][0][1]);
+                m_controller->ack_tx(c_r_ack[0][0][1]);
 
                 m_router[0][0]->out_flit[CORE](c_r_flit[i][j][0]);
                 m_router[0][0]->out_req[CORE](c_r_req[i][j][0]);
@@ -121,14 +131,17 @@ int sc_main(int argc, char *argv[])
                 m_router[0][0]->in_flit[CORE](c_r_flit[i][j][1]);
                 m_router[0][0]->in_req[CORE](c_r_req[i][j][1]);
                 m_router[0][0]->out_ack[CORE](c_r_ack[i][j][1]);
+
+                m_core[0][0]->flit_rx(dummy_flit[0]);
+                m_core[0][0]->req_rx(dummy_req[0]);
+                m_core[0][0]->ack_rx(dummy_ack[0]);
+
+                m_core[0][0]->flit_tx(dummy_flit[1]);
+                m_core[0][0]->req_tx(dummy_req[1]);
+                m_core[0][0]->ack_tx(dummy_ack[1]);
             }
             else
             {
-                // Connect rst and clk
-                m_core[i][j]->clk(clk);
-                m_core[i][j]->rst(rst);
-                m_router[i][j]->clk(clk);
-                m_router[i][j]->rst(rst);
 
                 // Connect Core & routers
                 m_core[i][j]->flit_rx(c_r_flit[i][j][0]);
